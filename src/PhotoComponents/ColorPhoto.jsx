76b942deed381150f159e49e9a566efebefd38d9
grumpiestcub <grumpiestcub@gmail.com>
2024-11-05 08:59:53 -0600

    re-sized color photos, created css page for photos

--- src/PhotoComponents/ColorPhoto.jsx
+++ src/PhotoComponents/ColorPhoto.jsx
@@ -2,13 +2,14 @@ import color1 from '/src/assets/color-images/color1.jpg'
 import color2 from '/src/assets/color-images/color2.jpg'
 import color3 from '/src/assets/color-images/color3.jpg'
 import color4 from '/src/assets/color-images/color4.jpg'
+import '/src/css/photos.css'
 
-
-
+// want to make this a gallery of some sort that cycles through the images, 
+// should be able to click on them and make them full screen,
+// may need to do more than some css for this. 
 
 function ColorPhotos() {
 const colorImages = [color1, color2, color3, color4]
-
  return (
     <>
     <div className='color-photos'>
